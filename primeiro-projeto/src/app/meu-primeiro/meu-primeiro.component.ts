7df591519fef35ebf6fc66a670fed749f4b9b936
daneccf <danielecavalcante1@hotmail.com>
2020-06-13 14:38:13 -0300

    first component

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component com Angular 2!</p>
+        `
+})
+export class MeuPrimeiroComponent { }
